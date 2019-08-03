a9bae8f2a7213ccfdd4e7d68d58b3284889db118
Diego Chinellato <chine.diego@gmail.com>
2019-08-03 15:38:20 +0200

    Diego test

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>VideoGen - The Next Generation</title>
+    <title>VideoGen - The Next Generation Market</title>
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
