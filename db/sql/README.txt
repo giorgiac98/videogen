ef430bf642b280bb9b992ba98d86a7a325dc5548
Giorgia Campardo <giorgiac98.gc@gmail.com>
2019-10-02 21:59:48 +0200

    new games

--- db/sql/README.txt
+++ db/sql/README.txt
@@ -1,3 +1,3 @@
 Importante: esclusi i 2 file "create_db" e "create_tables"
 il resto delle modifiche e fill apportati alla base di dati saranno
-marcati nel seguente modo "Step-ddmmyyyy-shortDescription.sql"
+marcati nel seguente modo "StepN-ddmmyyyy-shortDescription.sql"
---
+++ db/sql/Step4-02102019-fillTables.sql
@@ -0,0 +1,47 @@
+INSERT INTO public.videogiochi(titolo, produttore, img_path, prezzo, qta, console, descrizione)
+	VALUES
+		('FIFA 20', 'Electronic Arts', 'media\fifa20.jpg', 59.99, 10, 'PS4',
+		'EA SPORTS riporta il gioco nelle strade con la vera cultura, creatività e stile delle partite a ranghi ridotti. Crea il tuo personaggio e gioca secondo il tuo stile in diversi scenari in tutto il mondo.
+		Gameplay VOLTA: esprimi il tuo stile con un sistema di gioco completamente nuovo improntato al realismo. Ispirato al calcio a ranghi ridotti giocato nelle strade, nelle gabbie e nei campi di calcio a 5 di tutto il mondo.
+		Prova l''INTELLIGENZA CALCISTICA, che presenta un cambio di prospettiva tale da offrire un livello di realismo senza precedenti.
+		L''INTELLIGENZA CALCISTICA consente di vivere al massimo ogni attimo sul campo: con la palla, senza palla e attraverso la palla.'
+		),
+		('FIFA 20', 'Electronic Arts', 'media\fifa20.jpg', 69.99, 10, 'XBOX ONE',
+		'EA SPORTS riporta il gioco nelle strade con la vera cultura, creatività e stile delle partite a ranghi ridotti. Crea il tuo personaggio e gioca secondo il tuo stile in diversi scenari in tutto il mondo.
+		Gameplay VOLTA: esprimi il tuo stile con un sistema di gioco completamente nuovo improntato al realismo. Ispirato al calcio a ranghi ridotti giocato nelle strade, nelle gabbie e nei campi di calcio a 5 di tutto il mondo.
+		Prova l''INTELLIGENZA CALCISTICA, che presenta un cambio di prospettiva tale da offrire un livello di realismo senza precedenti.
+		L''INTELLIGENZA CALCISTICA consente di vivere al massimo ogni attimo sul campo: con la palla, senza palla e attraverso la palla.'
+		),
+		('FIFA 20', 'Electronic Arts', 'media\fifa20.jpg', 54.99, 10, 'PC',
+		'EA SPORTS riporta il gioco nelle strade con la vera cultura, creatività e stile delle partite a ranghi ridotti. Crea il tuo personaggio e gioca secondo il tuo stile in diversi scenari in tutto il mondo.
+		Gameplay VOLTA: esprimi il tuo stile con un sistema di gioco completamente nuovo improntato al realismo. Ispirato al calcio a ranghi ridotti giocato nelle strade, nelle gabbie e nei campi di calcio a 5 di tutto il mondo.
+		Prova l''INTELLIGENZA CALCISTICA, che presenta un cambio di prospettiva tale da offrire un livello di realismo senza precedenti.
+		L''INTELLIGENZA CALCISTICA consente di vivere al massimo ogni attimo sul campo: con la palla, senza palla e attraverso la palla.'
+		),
+		('Assassin''s Creed Origins', 'Ubisoft', 'media\ac_origins.jpg', 25.38, 10, 'PS4',
+		'L''origine di Bayek, il guardiano d’Egitto, la cui storia personale porterà alla nascita della confraternita
+		Un''avventura action-rpg prova un metodo di combattimento con ricompense e una vasta gamma di armi
+		Un''avventura action-RPG prova un metodo di combattimento con ricompense e una vasta gamma di armi
+		Assassin''s Creed Origins viene ottimizzato per offrire una esperienza di gioco a risoluzione 4K'
+		),
+		('Assassin''s Creed Origins', 'Ubisoft', 'media\ac_origins.jpg', 20.98, 10, 'XBOX ONE',
+		'L''origine di Bayek, il guardiano d’Egitto, la cui storia personale porterà alla nascita della confraternita
+		Un''avventura action-rpg prova un metodo di combattimento con ricompense e una vasta gamma di armi
+		Un''avventura action-RPG prova un metodo di combattimento con ricompense e una vasta gamma di armi
+		Assassin''s Creed Origins viene ottimizzato per offrire una esperienza di gioco a risoluzione 4K'
+		),
+		('Assassin''s Creed Origins', 'Ubisoft', 'media\ac_origins.jpg', 34.22, 10, 'PC',
+		'L''origine di Bayek, il guardiano d’Egitto, la cui storia personale porterà alla nascita della confraternita
+		Un''avventura action-rpg prova un metodo di combattimento con ricompense e una vasta gamma di armi
+		Un''avventura action-RPG prova un metodo di combattimento con ricompense e una vasta gamma di armi
+		Assassin''s Creed Origins viene ottimizzato per offrire una esperienza di gioco a risoluzione 4K'
+		),
+		('Far Cry 5', 'Ubisoft', 'media\far_cry5.jpg', 54.99, 15, 'PS4',
+		'Ecco Hope County, Montana, terra dei liberi, patria dei coraggiosi... e della setta apocalittica Progetto dell''Eden''s Gate. Affronta il capo spirituale Joseph Seed e i suoi fratelli, gli Araldi: accendi il fuoco della ribellione e libera una comunità sotto assedio. AFFRONTA UNA SETTA LETALE Libera Hope County in modalità a un giocatore o in due. Recluta mercenari e addestra animali per sconfiggere la setta. UN MONDO CHE CONTRATTACCA Semina il caos nella setta, ma occhio a non far infuriare Joseph Seed e i suoi seguaci. SCEGLI LA TUA STRADA Crea il personaggio e scegli la tua avventura nel capitolo più personalizzabile di Far Cry. MEZZI DINAMICI Guida famose muscle car, quad, aerei e molto altro mentre affronti le truppe della setta in scontri epici.'
+		),
+		('Far Cry 5', 'Ubisoft', 'media\far_cry5.jpg', 69.99, 15, 'XBOX ONE',
+		'Ecco Hope County, Montana, terra dei liberi, patria dei coraggiosi... e della setta apocalittica Progetto dell''Eden''s Gate. Affronta il capo spirituale Joseph Seed e i suoi fratelli, gli Araldi: accendi il fuoco della ribellione e libera una comunità sotto assedio. AFFRONTA UNA SETTA LETALE Libera Hope County in modalità a un giocatore o in due. Recluta mercenari e addestra animali per sconfiggere la setta. UN MONDO CHE CONTRATTACCA Semina il caos nella setta, ma occhio a non far infuriare Joseph Seed e i suoi seguaci. SCEGLI LA TUA STRADA Crea il personaggio e scegli la tua avventura nel capitolo più personalizzabile di Far Cry. MEZZI DINAMICI Guida famose muscle car, quad, aerei e molto altro mentre affronti le truppe della setta in scontri epici.'
+		),
+		('Far Cry 5', 'Ubisoft', 'media\far_cry5.jpg', 49.99, 15, 'PC',
+		'Ecco Hope County, Montana, terra dei liberi, patria dei coraggiosi... e della setta apocalittica Progetto dell''Eden''s Gate. Affronta il capo spirituale Joseph Seed e i suoi fratelli, gli Araldi: accendi il fuoco della ribellione e libera una comunità sotto assedio. AFFRONTA UNA SETTA LETALE Libera Hope County in modalità a un giocatore o in due. Recluta mercenari e addestra animali per sconfiggere la setta. UN MONDO CHE CONTRATTACCA Semina il caos nella setta, ma occhio a non far infuriare Joseph Seed e i suoi seguaci. SCEGLI LA TUA STRADA Crea il personaggio e scegli la tua avventura nel capitolo più personalizzabile di Far Cry. MEZZI DINAMICI Guida famose muscle car, quad, aerei e molto altro mentre affronti le truppe della setta in scontri epici.'
+		);
