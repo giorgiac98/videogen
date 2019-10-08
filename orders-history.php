e1f809a63fe35a9ff43afde09d2fec3f32820aac
LLAgu <lagunasluca@protonmail.com>
2019-10-08 11:02:22 +0200

    momenti produttivi durante le lezioni di Cortesi

--- orders-history.php
+++ orders-history.php
@@ -37,8 +37,12 @@
             </thead>
             <tbody>
                <?php
-               $query = $db->prepare("SELECT DISTINCT id,id_utente,data FROM ordini ORDER BY data");
+
+               $query = $db->prepare(" SELECT ordini.id AS id,id_utente,data,titolo,prezzo,(prezzo*ordini_giochi.qta) AS tot
+                                        FROM videogiochi,utenti,ordini,ordini_giochi
+                                        WHERE id_utente = utenti.id AND id_ordine = ordini.id AND id_gioco = videogiochi.id");
                $query->execute();
+
                while ($games = $query->fetch(PDO::FETCH_ASSOC)) {
                  echo'<tr><td>';
                  echo '<div class="col-sm-4">'. $games['id'] . '</div>';
@@ -53,12 +57,16 @@
                     <a data-toggle="collapse" href="#collapse'. $games['id'] . '">Visualizza prodotti</a>
                   </h4>
                  </div>
-                    <div id="collapse'. $games['id'] . '" class="panel-collapse collapse">
-                      <div class="col-sm-8">prodotto 1</div>
-                      <div class="col-sm-8">prodotto 2</div>
-                 </div></div>';
+                    <div id="collapse'. $games['id'] . '" class="panel-collapse collapse">';
+
+
+                echo '<div class="col-sm-8">' . $games['titolo'] . '</div>';
+
+
+                 echo '</div></div>';
                  echo '</td><td>';
-                 echo "$$$";
+
+                 echo '<div class="col-sm-8">' . $games['tot'] . '</div>';
                  echo '</td></tr>';
 
                }
@@ -73,38 +81,4 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
-  <!--<p><center>Premi il numero ordine per visualizzare i prodotti ordinati</center></p>-->
-</html>
-
-
-
-
-<!--
-<div class="accordion" id="accordionExample">
-  <div class="card">
-    <div class="card-header" id="headingOne">
-      <h2 class="mb-0">
-        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Prodotti
-        </button>
-      </h2>
-    </div>
-
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div class="card-body">
-        Prodotto 1
-        Prodotto 2
-        Prodotto 3
-        Prodotto n
-      </div>
-    </div>
--->
-
-<!-- while ($games = $query->fetch(PDO::FETCH_ASSOC)) { -->
-
-<!--      <div class="row">
-           <div class="col-sm-4">ID ordine</div>
-           <div class="col-sm-4">Data</div>
-           <div class="col-sm-4">Totale</div>
-         </div>
--->
+</html><!--->>
