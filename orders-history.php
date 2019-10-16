d2d6636dd045dc9400b7745274d6e9dc58358a6e
Giorgia Campardo <giorgiac98.gc@gmail.com>
2019-10-17 00:24:56 +0200

    Sistemato le query TODO sistemare la grafica

--- orders-history.php
+++ orders-history.php
@@ -30,53 +30,58 @@
             <thead>
               <tr>
                 <th>Numero ordine </th>
-                <th>Utente</th>
                 <th>Data ordine</th>
+                <th>Tipo di Pagamento</th>
                 <th>Prodotti</th>
                 <th>Totale</th>
               </tr>
             </thead>
             <tbody>
                <?php
-
-               $query = $db->prepare("  SELECT data,ordini.id AS id,id_utente,totale
-                                        FROM videogiochi,utenti,ordini,ordini_giochi
-                                        WHERE id_utente = utenti.id AND id_ordine = ordini.id AND id_gioco = videogiochi.id AND utenti.username = ?
-                                        GROUP BY data,ordini.id,id_utente,totale");
+               /* TODO assigned by Giorgia
+               * ti ho sistemato le query perché prendevi su roba che non serviva e
+               * facevi dei GROUP BY inutili che appesantivano solo la query
+               * ho sistemato qualche nome di variabile per essere più precisi
+               * ti chiedo di sistemare bene la grafica : TIPS dentro al tag <td>
+               * non serve per forza mettere un div, per cambiare l'ampiezza della colonna
+               * basta usare <td colspan="2"> per avere il campo che occupa 2 colonne
+               */
+               $query = $db->prepare("SELECT o.id, data, tipo_pagamento, totale
+                                      FROM ordini o JOIN utenti u ON u.id = o.id_utente
+                                      WHERE u.username = ?");
                $query->execute([$_SESSION['user']]);
 
-               $query2 = $db->prepare("  SELECT titolo,prezzo,ordini_giochi.qta AS num,console
-                         FROM videogiochi,utenti,ordini,ordini_giochi
-                         WHERE id_utente = utenti.id AND id_ordine = ordini.id AND id_gioco = videogiochi.id AND utenti.username = ? AND data = ?");
+               $query2 = $db->prepare("SELECT id_gioco, titolo, prezzo, console, og.qta as qta
+                                       FROM ordini_giochi og JOIN videogiochi v on v.id = og.id_gioco
+                                       WHERE id_ordine = ?");
 
-
-               while ($games = $query->fetch(PDO::FETCH_ASSOC)) {
+               while ($order = $query->fetch(PDO::FETCH_ASSOC)) {
 
                  echo'<tr><td>';
-                 echo '<div class="col-sm-2">'. $games['id'] . '</div>';
+                 echo '<div class="col-sm-2">'. $order['id'] . '</div>';
                  echo '</td><td>';
-                 echo '<div class="col-sm-2">'. $games['id_utente'] . '</div>';
+                 echo '<div class="col-sm-8">'. $order['data'] . '</div>';
                  echo '</td><td>';
-                 echo '<div class="col-sm-4">'. $games['data'] . '</div>';
+                 echo '<div class="col-sm-8">'. $order['tipo_pagamento'] . '</div>';
                  echo '</td><td>';
                  echo '<div class="col-sm-16">
                  <div class="panel-heading">
 
                   <h4 class="panel-title">
-                    <a data-toggle="collapse" href="#collapse'. $games['id'] . '">Visualizza prodotti</a>
+                    <a data-toggle="collapse" href="#collapse'. $order['id'] . '">Visualizza prodotti</a>
                   </h4>
                  </div>
-                    <div id="collapse'. $games['id'] . '" class="panel-collapse collapse">';
+                    <div id="collapse'. $order['id'] . '" class="panel-collapse collapse">';
 
-                $query2->execute([$_SESSION['user'],$games['data']]);
-                while ($games2 = $query2->fetch(PDO::FETCH_ASSOC)){
-                  echo '<div class="col-sm-16">' . $games2['titolo'],'  ','(',$games2['console'],')','   x',$games2['num']. '</div>';
+                $query2->execute([$order['id']]);
+                while ($game = $query2->fetch(PDO::FETCH_ASSOC)){
+                  echo '<div class="col-sm-16">' . $game['titolo'] . '  ' . '(' . $game['console'] . ')' . '   x' . $game['qta'] . '</div>';
                 }
 
                  echo '</div></div>';
                  echo '</td><td>';
                  //$tmp = $games['totale'];
-                 echo '<div class="col-sm-8">' . /*bcmul($tmp,1,2)*/$games['totale']  . '</div>';
+                 echo '<div class="col-sm-8">€ ' . /*bcmul($tmp,1,2)*/$order['totale']  . '</div>';
                  echo '</td></tr>';
 
                }
