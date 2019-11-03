af4df909c9252d880a82eccb6398a7a7bcbc9e0b
giorgiac98 <giorgiac98.gc@gmail.com>
2019-11-03 16:21:20 +0100

    New games for project presentation

---
+++ db/sql/Step5-03112019-fillTables.sql
@@ -0,0 +1,80 @@
+INSERT INTO public.videogiochi(titolo, produttore, img_path, prezzo, qta, console, descrizione)
+	VALUES
+		('Red Dead Redemption 2', 'Rockstar Games', 'media\red_dead_redemption2.jpg', 39.99, 10, 'PS4',
+		'America, 1899. L''era del selvaggio West è agli sgoccioli: la legge sta catturando le ultime bande di fuorilegge. Chi rifiuta di arrendersi viene ucciso senza pietà.
+		Dopo un colpo andato storto nella città di Blackwater, Arthur Morgan e la banda di Van der Linde sono costretti alla fuga. Con gli agenti federali e i migliori cacciatori di taglie alle costole, la banda deve rapinare, combattere e rubare per farsi strada e cercare di sopravvivere nel cuore di un''America dura e selvaggia.
+		Una serie di conflitti e divisioni rischia di mettere a repentaglio l''unità del gruppo e Arthur si ritrova costretto a scegliere tra i suoi ideali e la lealtà nei confronti della banda che l''ha cresciuto.
+		Dai creatori di Grand Theft Auto V e Red Dead Redemption, Red Dead Redemption 2 è una storia epica che ci mostra un''America agli albori della modernità.'
+		),
+		('Red Dead Redemption 2', 'Rockstar Games', 'media\red_dead_redemption2.jpg', 34.99, 10, 'XBOX ONE',
+		'America, 1899. L''era del selvaggio West è agli sgoccioli: la legge sta catturando le ultime bande di fuorilegge. Chi rifiuta di arrendersi viene ucciso senza pietà.
+		Dopo un colpo andato storto nella città di Blackwater, Arthur Morgan e la banda di Van der Linde sono costretti alla fuga. Con gli agenti federali e i migliori cacciatori di taglie alle costole, la banda deve rapinare, combattere e rubare per farsi strada e cercare di sopravvivere nel cuore di un''America dura e selvaggia.
+		Una serie di conflitti e divisioni rischia di mettere a repentaglio l''unità del gruppo e Arthur si ritrova costretto a scegliere tra i suoi ideali e la lealtà nei confronti della banda che l''ha cresciuto.
+		Dai creatori di Grand Theft Auto V e Red Dead Redemption, Red Dead Redemption 2 è una storia epica che ci mostra un''America agli albori della modernità.'
+		),
+		('The Sims 4', 'Electronic Arts', 'media\the_sims4.jpg', 19.99, 10, 'PS4',
+		'Goditi il potere di creare e controllare personaggi in un mondo virtuale senza regole. Esprimi la tua creatività personalizzando le loro distinte caratteristiche e personalità uniche. Decidi il loro stile, scegli il loro taglio, e dagli delle aspirazioni.
+		Costruisci senza fatica case perfette ai tuoi Sims con il nuovo Build Mode delle stanze, scegliendo i tuoi design e arredamenti preferiti. Sviluppa le relazioni dei tuoi Sims, persegui nuove carriere, e plasma gli stimolanti e ricchi momenti delle loro vite.
+		Esplora bellissimi mondi con ambienti unici e viaggia in altri quartieri dove potrai visitare e incontrare altri Sims. Goditi i tuoi poteri e libertà, divertiti, e gioca con la vita!'
+		),
+		('The Sims 4', 'Electronic Arts', 'media\the_sims4.jpg', 19.99, 10, 'XBOX ONE',
+		'Goditi il potere di creare e controllare personaggi in un mondo virtuale senza regole. Esprimi la tua creatività personalizzando le loro distinte caratteristiche e personalità uniche. Decidi il loro stile, scegli il loro taglio, e dagli delle aspirazioni.
+		Costruisci senza fatica case perfette ai tuoi Sims con il nuovo Build Mode delle stanze, scegliendo i tuoi design e arredamenti preferiti. Sviluppa le relazioni dei tuoi Sims, persegui nuove carriere, e plasma gli stimolanti e ricchi momenti delle loro vite.
+		Esplora bellissimi mondi con ambienti unici e viaggia in altri quartieri dove potrai visitare e incontrare altri Sims. Goditi i tuoi poteri e libertà, divertiti, e gioca con la vita!'
+		),
+		('The Sims 4', 'Electronic Arts', 'media\the_sims4.jpg', 19.99, 10, 'PC',
+		'Goditi il potere di creare e controllare personaggi in un mondo virtuale senza regole. Esprimi la tua creatività personalizzando le loro distinte caratteristiche e personalità uniche. Decidi il loro stile, scegli il loro taglio, e dagli delle aspirazioni.
+		Costruisci senza fatica case perfette ai tuoi Sims con il nuovo Build Mode delle stanze, scegliendo i tuoi design e arredamenti preferiti. Sviluppa le relazioni dei tuoi Sims, persegui nuove carriere, e plasma gli stimolanti e ricchi momenti delle loro vite.
+		Esplora bellissimi mondi con ambienti unici e viaggia in altri quartieri dove potrai visitare e incontrare altri Sims. Goditi i tuoi poteri e libertà, divertiti, e gioca con la vita!'
+		),
+		('Star Wars Jedi Fallen Order', 'Electronic Arts', 'media\star_wars.jpg', 61.99, 10, 'PS4',
+		'Una nuova avventura galattica ti attende in Star Wars Jedi: Fallen Order, un gioco d''azione e d''avventura in 3a persona sviluppato da Respawn Entertainment. In questa avventura per giocatore singolo dalla forte componente narrativa assumi il ruolo di un Padawan Jedi sopravvissuto all''epurazione dell''Ordine 66, subito dopo gli eventi di Episodio III: La Vendetta dei Sith.
+		Deciso a ricostruire l''Ordine Jedi, devi raccogliere i frammenti del tuo passato in frantumi per completare l''addestramento, sviluppare nuove potenti abilità della Forza e padroneggiare l''arte dell''iconica spada laser, tutto questo senza farti sopraffare dall''Impero e dai suoi micidiali Inquisitori.
+		Perfezionando le tue abilità, ti ritroverai a combattere intensi e spettacolari duelli a colpi di spada laser e a scatenare i poteri della Forza, esattamente come nei film della saga di Star Wars. Dovrai affrontare i nemici strategicamente, valutandone punti di forza e debolezze e al tempo stesso usando con astuzia il tuo addestramento Jedi per batterli e risolvere i misteri lungo il tuo cammino.'
+		),
+		('Star Wars Jedi Fallen Order', 'Electronic Arts', 'media\star_wars.jpg', 61.99, 10, 'XBOX ONE',
+		'Una nuova avventura galattica ti attende in Star Wars Jedi: Fallen Order, un gioco d''azione e d''avventura in 3a persona sviluppato da Respawn Entertainment. In questa avventura per giocatore singolo dalla forte componente narrativa assumi il ruolo di un Padawan Jedi sopravvissuto all''epurazione dell''Ordine 66, subito dopo gli eventi di Episodio III: La Vendetta dei Sith.
+		Deciso a ricostruire l''Ordine Jedi, devi raccogliere i frammenti del tuo passato in frantumi per completare l''addestramento, sviluppare nuove potenti abilità della Forza e padroneggiare l''arte dell''iconica spada laser, tutto questo senza farti sopraffare dall''Impero e dai suoi micidiali Inquisitori.
+		Perfezionando le tue abilità, ti ritroverai a combattere intensi e spettacolari duelli a colpi di spada laser e a scatenare i poteri della Forza, esattamente come nei film della saga di Star Wars. Dovrai affrontare i nemici strategicamente, valutandone punti di forza e debolezze e al tempo stesso usando con astuzia il tuo addestramento Jedi per batterli e risolvere i misteri lungo il tuo cammino.'
+		),
+		('Star Wars Jedi Fallen Order', 'Electronic Arts', 'media\star_wars.jpg', 58.99, 10, 'PC',
+		'Una nuova avventura galattica ti attende in Star Wars Jedi: Fallen Order, un gioco d''azione e d''avventura in 3a persona sviluppato da Respawn Entertainment. In questa avventura per giocatore singolo dalla forte componente narrativa assumi il ruolo di un Padawan Jedi sopravvissuto all''epurazione dell''Ordine 66, subito dopo gli eventi di Episodio III: La Vendetta dei Sith.
+		Deciso a ricostruire l''Ordine Jedi, devi raccogliere i frammenti del tuo passato in frantumi per completare l''addestramento, sviluppare nuove potenti abilità della Forza e padroneggiare l''arte dell''iconica spada laser, tutto questo senza farti sopraffare dall''Impero e dai suoi micidiali Inquisitori.
+		Perfezionando le tue abilità, ti ritroverai a combattere intensi e spettacolari duelli a colpi di spada laser e a scatenare i poteri della Forza, esattamente come nei film della saga di Star Wars. Dovrai affrontare i nemici strategicamente, valutandone punti di forza e debolezze e al tempo stesso usando con astuzia il tuo addestramento Jedi per batterli e risolvere i misteri lungo il tuo cammino.'
+		),
+		('Destiny 2', 'ACTIVISION', 'media\destiny2.jpg', 9.99, 10, 'PS4',
+		'Dai creatori dell''acclamato Destiny arriva l''atteso seguito: uno sparatutto d''azione che ti accompagnerà in un epico viaggio alla scoperta del sistema solare.
+		L''ultima città sicura sulla Terra è caduta sotto i colpi degli invasori guidati da Ghaul, l''imponente comandante della brutale Legione Rossa che ha privato i guardiani del loro potere e costretto i superstiti alla fuga.
+		Ti avventurerai nel nostro sistema solare, in mondi misteriosi e inesplorati allo scopo di trovare un arsenale di armi e devastanti, nuovissime abilità.
+		Per sconfiggere la Legione Rossa e affrontare Ghaul, dovrai radunare gli eroi dispersi e combattere per riconquistare la nostra terra.'
+		),
+		('Destiny 2', 'ACTIVISION', 'media\destiny2.jpg', 21.00, 10, 'XBOX ONE',
+		'Dai creatori dell''acclamato Destiny arriva l''atteso seguito: uno sparatutto d''azione che ti accompagnerà in un epico viaggio alla scoperta del sistema solare.
+		L''ultima città sicura sulla Terra è caduta sotto i colpi degli invasori guidati da Ghaul, l''imponente comandante della brutale Legione Rossa che ha privato i guardiani del loro potere e costretto i superstiti alla fuga.
+		Ti avventurerai nel nostro sistema solare, in mondi misteriosi e inesplorati allo scopo di trovare un arsenale di armi e devastanti, nuovissime abilità.
+		Per sconfiggere la Legione Rossa e affrontare Ghaul, dovrai radunare gli eroi dispersi e combattere per riconquistare la nostra terra.'
+		),
+		('Destiny 2', 'ACTIVISION', 'media\destiny2.jpg', 12.70, 10, 'PC',
+		'Dai creatori dell''acclamato Destiny arriva l''atteso seguito: uno sparatutto d''azione che ti accompagnerà in un epico viaggio alla scoperta del sistema solare.
+		L''ultima città sicura sulla Terra è caduta sotto i colpi degli invasori guidati da Ghaul, l''imponente comandante della brutale Legione Rossa che ha privato i guardiani del loro potere e costretto i superstiti alla fuga.
+		Ti avventurerai nel nostro sistema solare, in mondi misteriosi e inesplorati allo scopo di trovare un arsenale di armi e devastanti, nuovissime abilità.
+		Per sconfiggere la Legione Rossa e affrontare Ghaul, dovrai radunare gli eroi dispersi e combattere per riconquistare la nostra terra.'
+		),
+		('Rocket League', 'Warner Bros', 'media\rocket_league.jpg', 17.98, 10, 'PS4',
+		'Rocket League mette a disposizione dei giocatori dei veicoli ultra potenziati che possono essere scagliati contro i palloni per produrre goal o salvataggi epici in varie e super dettagliate arene di gioco.
+		Scegli la tua preferita tra tante auto diverse e personalizzala a tuo piacimento prima di affrontare i tuoi avversari in questo sport che mescola calcio e destruction derby.
+		Vola in alto per effettuare colpi acrobatici e salvataggi impensabili, oppure gioca aggressivamente e abbatti i tuoi nemici con cariche a velocità supersonica.
+		Scegli tra tanti mezzi acrobatici equipaggiati con fantastici turbo. Librati in volo per segnare incredibili gol aerei e cambia l’esito della partita con dei salvataggi straordinari.'
+		),
+		('Rocket League', 'Warner Bros', 'media\rocket_league.jpg', 18.65, 10, 'XBOX ONE',
+		'Rocket League mette a disposizione dei giocatori dei veicoli ultra potenziati che possono essere scagliati contro i palloni per produrre goal o salvataggi epici in varie e super dettagliate arene di gioco.
+		Scegli la tua preferita tra tante auto diverse e personalizzala a tuo piacimento prima di affrontare i tuoi avversari in questo sport che mescola calcio e destruction derby.
+		Vola in alto per effettuare colpi acrobatici e salvataggi impensabili, oppure gioca aggressivamente e abbatti i tuoi nemici con cariche a velocità supersonica.
+		Scegli tra tanti mezzi acrobatici equipaggiati con fantastici turbo. Librati in volo per segnare incredibili gol aerei e cambia l’esito della partita con dei salvataggi straordinari.'
+		),
+		('Rocket League', 'Warner Bros', 'media\rocket_league.jpg', 22.37, 10, 'Nintendo Switch',
+		'Rocket League mette a disposizione dei giocatori dei veicoli ultra potenziati che possono essere scagliati contro i palloni per produrre goal o salvataggi epici in varie e super dettagliate arene di gioco.
+		Scegli la tua preferita tra tante auto diverse e personalizzala a tuo piacimento prima di affrontare i tuoi avversari in questo sport che mescola calcio e destruction derby.
+		Vola in alto per effettuare colpi acrobatici e salvataggi impensabili, oppure gioca aggressivamente e abbatti i tuoi nemici con cariche a velocità supersonica.
+		Scegli tra tanti mezzi acrobatici equipaggiati con fantastici turbo. Librati in volo per segnare incredibili gol aerei e cambia l’esito della partita con dei salvataggi straordinari.'
+		);
